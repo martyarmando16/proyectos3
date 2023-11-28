55a0394ce891f92a717875cd63387222d84e2377
MartyDev <martinarmando16@gmail.com>
2023-11-28 16:04:11 -0500

    Archivo python actualiado localmente

--- hello.py
+++ hello.py
@@ -1,3 +1,32 @@
 """
 Esto es el inicio
 """
+d1 = {
+    "Nombre": "Jorgue",
+    "Edad": 25,
+    "Documento": 10203040
+}
+
+#1. Imprimir los key de los diccionarios
+for x in d1:
+    print(x)
+#Nombre
+#Edad
+#Documento
+
+#2. Imprimir los values del diccionario
+for x in d1:
+    print(d1[x])
+#Jorgue
+#25
+#10203040
+
+#3. Imprimir los keys y values del diccionario
+
+for x, y in d1.items():
+    print(x, y)
+#Nombre Jorgue
+#Edad 25
+#Documento 10203040
+
+#print(d1.items())
